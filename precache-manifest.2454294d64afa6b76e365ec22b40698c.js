b2e72e2762fff987b40661571d9d4daef1b1c950
Roman Volman <you@example.com>
2020-05-10 00:26:03 +0300

    Updates

---
+++ precache-manifest.2454294d64afa6b76e365ec22b40698c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fff6340aca6b0c295f84407d82dabac",
+    "url": "/Monstars-2/index.html"
+  },
+  {
+    "revision": "db575ab5a2d75194fb4a",
+    "url": "/Monstars-2/static/css/main.8c324b26.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/Monstars-2/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monstars-2/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db575ab5a2d75194fb4a",
+    "url": "/Monstars-2/static/js/main.303f72d8.chunk.js"
+  },
+  {
+    "revision": "3d9b15df3429eeaf780f",
+    "url": "/Monstars-2/static/js/runtime-main.3edcaff5.js"
+  }
+]);
